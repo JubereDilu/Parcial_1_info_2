4f4a5a5519e6ecf26bbb076ab89204a67a79ce6b
JubereDilu <dajumuriel@gmail.com>
2023-09-23 12:30:41 -0500

    Escribo un pequeño programa para probar la matriz led

--- main.cpp
+++ main.cpp
@@ -15,9 +15,20 @@ void setup() {
     pinMode(SRCLK2, OUTPUT);
     pinMode(RCLK2, OUTPUT);
 
-    // Vamos a escribir por estos pines
+    // Inicializamos los pines por los que vamos a escribir
     digitalWrite(RCLK1, LOW);
-    digitalWrite(SRCLK1, LOW);
     digitalWrite(RCLK2, LOW);
-    digitalWrite(SRCLK2, LOW);
+
+}
+
+void loop() {
+    // Encender todos los LEDs
+
+    shiftOut(SER1, SRCLK1, MSBFIRST, 255); //Se cargan los datos en el registro de desplazamiento
+
+    digitalWrite(RCLK1, HIGH); // Latch para actualizar la matriz (cargar datos al registro de almacenamiento)
+    digitalWrite(RCLK1, LOW); // actualiza y muestra correctamente los datos en la matriz
+
+
+    delay(1000); // Espera 1 segundo
 }
