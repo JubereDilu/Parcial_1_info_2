433eb6ab7b0f3776749dde04b171be9c59f7eec3
JubereDilu <dajumuriel@gmail.com>
2023-09-22 22:26:48 -0500

    Defino y configuro los pines de los 74HC595

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
+// Definición de pines
+#define SER1 11
+#define SRCLK1 12
+#define RCLK1 8
+#define SER2 7
+#define SRCLK2 13
+#define RCLK2 9
 
-using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
-}
+void setup() {
+    // Configuración de pines como salidas
+    pinMode(SER1, OUTPUT);
+    pinMode(SRCLK1, OUTPUT);
+    pinMode(RCLK1, OUTPUT);
+    pinMode(SER2, OUTPUT);
+    pinMode(SRCLK2, OUTPUT);
+    pinMode(RCLK2, OUTPUT);
