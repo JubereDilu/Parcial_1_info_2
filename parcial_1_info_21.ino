002041f45d458742874d21f258aa23287ae4fa70
JubereDilu <145413403+JubereDilu@users.noreply.github.com>
2023-09-24 23:39:46 -0500

    probando código
    Pruebo  código para ver si consigo imprimir los patrones , estoy desesperado nada me funciona, borre parte de lo que tenia

--- parcial_1_info_21.ino
+++ parcial_1_info_21.ino
@@ -1,12 +1,26 @@
 // Definición de pines
-#define SER1 11
-#define SRCLK1 12
-#define RCLK1 8
-#define SER2 7
-#define SRCLK2 13
-#define RCLK2 9
+#define SER1   8  
+#define RCLK1   9  
+#define SRCLK1   10  
+
+//#define SER2 8    4
+//#define RCLK2 9    5
+//#define SRCLK2 10   6
+
+// data 8
+//store 9 
+//shift 10
 
 
+int j = 0;
+r
+int k;
+
+int row[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+
+int columnH[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+
+int matriz[8][8];
 
 void setup() {
     Serial.begin(9600);
@@ -15,26 +29,29 @@ void setup() {
     pinMode(SER1, OUTPUT);
     pinMode(SRCLK1, OUTPUT);
     pinMode(RCLK1, OUTPUT);
-    pinMode(SER2, OUTPUT);
-    pinMode(SRCLK2, OUTPUT);
-    pinMode(RCLK2, OUTPUT);
+   // pinMode(SER2, OUTPUT);
+   //pinMode(SRCLK2, OUTPUT);
+   // pinMode(RCLK2, OUTPUT);
     
     // Inicializamos los pines por los que vamos a escribir
     digitalWrite(RCLK1, LOW);
-    digitalWrite(RCLK2, LOW);
+    //digitalWrite(RCLK2, LOW);
      
 }
 
 void loop() {
   
   verificacion(); // Llamamos a la función de verificación
- 
+  
+  
 }
 
 void verificacion() {  
-  
+    
+    shiftOut(SER1, SRCLK1, LSBFIRST, columnH[0]);
+    shiftOut(SER1, SRCLK1, LSBFIRST, row[0]);
   // Encender todos los LEDs
-    shiftOut(SER1, SRCLK1, MSBFIRST, 255); //Se cargan los datos en el registro de desplazamiento
+    //shiftOut(SER1, SRCLK1, MSBFIRST, 255);  //Se cargan los datos en el registro de desplazamiento
     digitalWrite(RCLK1, HIGH); // Latch para actualizar la matriz (cargar datos al registro de almacenamiento)
     digitalWrite(RCLK1, LOW); // actualiza y muestra correctamente los datos en la matriz
   
@@ -48,17 +65,4 @@ void verificacion() {
     delay(2000); // Espera 2 segundos (todos los LEDs apagados)
    
 }
-void imagen() {
-    byte patrones[8]; // Array para almacenar el patrón de cada fila
-    byte fila;
 
-    for (fila = 0; fila < 8; fila++) {
-        Serial.print("Ingrese el patrón para la fila ");
-        Serial.print(fila);
-        Serial.print(": ");
-
-
-    }
-
-
-}
