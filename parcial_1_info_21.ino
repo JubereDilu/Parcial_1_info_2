45dc51f25e781804330f173996226f63b21cf47b
JubereDilu <145413403+JubereDilu@users.noreply.github.com>
2023-09-25 21:50:29 -0500

    Codigo final
    en el código esta la función verificación , 4 funciones mas que corresponden a los patrones y tengo un convertidor de binario a decimal. Las funciones de los patrones y verificación funcionan pero no como lo especifica el parcial (para probar una función tienes que descomentarla). Intente resolverlo pero me quede corto de tiempo.

--- parcial_1_info_21.ino
+++ parcial_1_info_21.ino
@@ -3,17 +3,9 @@
 #define RCLK1   9  
 #define SRCLK1   10  
 
-//#define SER2 8    4
-//#define RCLK2 9    5
-//#define SRCLK2 10   6
-
-// data 8
-//store 9 
-//shift 10
-
-
-
-
+unsigned long tiempoInicio = 0;//guarda tiempo de referencia 
+int segundos = 500; 
+  
 void setup() {
     Serial.begin(9600);
   
@@ -21,140 +13,286 @@ void setup() {
     pinMode(SER1, OUTPUT);
     pinMode(SRCLK1, OUTPUT);
     pinMode(RCLK1, OUTPUT);
-   // pinMode(SER2, OUTPUT);
-   //pinMode(SRCLK2, OUTPUT);
-   // pinMode(RCLK2, OUTPUT);
     
     // Inicializamos los pines por los que vamos a escribir
-    digitalWrite(RCLK1, LOW);
-    //digitalWrite(RCLK2, LOW);
-     
+    digitalWrite(RCLK1, LOW);     
 }
 
+  
 void loop() {
   
- //verificacion(); // Llamamos a la función de verificación
-  //patron1();
-  //patron2();
-  //patron3();
-  patron4();
+  // DESCOMENTA LO QUE QUIERAS PROBAR 
+  // LO INTENTE PERO NO PUDE, ME QUEDO GRANDE  =( 
   
+  //verificacion(); // Llamamos a la función de verificación
+  //patron1();
+  //patron2(); //llamamos la funcion que muestra el pantron X
+  //patron3();
+  //patron4();
 }
+
+int binarioADecimal(int fila[], int longitud) {
+    int decimal = 0;
+    int peso = 1; // Inicializar el peso en 2^0
+    for (int i = longitud - 1; i >= 0; i--) {
+        decimal += fila[i] * peso;
+        peso *= 2; // Multiplicar el peso por 2 para el próximo bit
+    }
+    return decimal;
+}
+  
 
 void verificacion() {  
     
-	digitalWrite(RCLK1, LOW);
-    shiftOut(SER1, SRCLK1, MSBFIRST, 255);  //Se cargan los datos en el registro de desplazamiento
-    //digitalWrite(RCLK1, HIGH); // Latch para actualizar la matriz (cargar datos al registro de almacenamiento)
-    //digitalWrite(RCLK1, LOW); // actualiza y muestra correctamente los datos en la matriz
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, 0);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 254 );  
+      digitalWrite(RCLK1, HIGH); 
   
-   	delay(2000); // Espera 2 segundos (todos los LEDs encendidos)
-    
-  // Apagar todos los LEDs
- 	//digitalWrite(RCLK1, LOW);
-   	shiftOut(SER1, SRCLK1, MSBFIRST, 0); // Se apagan los LEDs en el primer registro de desplazamiento     
-    digitalWrite(RCLK1, HIGH); // Latch para apagar la matriz (cargar datos al registro de almacenamiento)
-    //digitalWrite(RCLK1, LOW); // actualiza y muestra correctamente los datos en la matriz
-   
-    delay(2000); // Espera 2 segundos (todos los LEDs apagados)
-   
+    // Declarar una matriz 8x8 e inicializarla con 1
+    int matriz[8][8];
+
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            matriz[i][j] = 1;
+        }
+    }
+ 	// Declarar un arreglo de variables para almacenar los números decimales
+    int numerosDecimales[8];
+
+    // Calcular y almacenar los números decimales
+    for (int i = 0; i < 8; i++) {
+        numerosDecimales[i] = binarioADecimal(matriz[i], 8);
+    }
+      
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST,numerosDecimales[0]  );  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 254 );  
+      digitalWrite(RCLK1, HIGH); 
+
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST,numerosDecimales[1]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST,  253);  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[2]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 251 );  
+      digitalWrite(RCLK1, HIGH);
+
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[3]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 247 );  
+      digitalWrite(RCLK1, HIGH); 
+ 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[4]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST,239 );  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[5]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 223);  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[6]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST,191 );  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[7]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 127);  
+      digitalWrite(RCLK1, HIGH);
 }
 
-void patron2() {  
-    
-	digitalWrite(RCLK1, LOW); // actualiza y muestra correctamente los datos en la matriz
-    shiftOut(SER1, SRCLK1, LSBFIRST, B10000001);  //Se cargan los datos en el registro de desplazamiento
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B01111110);  
-    digitalWrite(RCLK1, HIGH); // Latch para actualizar la matriz (cargar datos al registro de almacenamiento)
+void patron2(){
   
- 	digitalWrite(RCLK1, LOW);// actualiza y muestra correctamente los datos en la matriz
-   	shiftOut(SER1, SRCLK1, LSBFIRST, B01000010); // Se apagan los LEDs en el primer registro de desplazamiento 
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B10111101);
-    digitalWrite(RCLK1, HIGH); // Latch para apagar la matriz (cargar datos al registro de almacenamiento)
-     
-    digitalWrite(RCLK1, LOW); // actualiza y muestra correctamente los datos en la matriz
-    shiftOut(SER1, SRCLK1, LSBFIRST, B00100100);  //Se cargan los datos en el registro de desplazamiento
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B11011011);  
-    digitalWrite(RCLK1, HIGH); // Latch para actualizar la matriz (cargar datos al registro de almacenamiento)
-    
-    digitalWrite(RCLK1, LOW); // actualiza y muestra correctamente los datos en la matriz
-    shiftOut(SER1, SRCLK1, LSBFIRST, B00011000);  //Se cargan los datos en el registro de desplazamiento
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B11100111);  
-    digitalWrite(RCLK1, HIGH); // Latch para actualizar la matriz (cargar datos al registro de almacenamiento)
-       
-}
-void patron1() {  
-    
-	digitalWrite(RCLK1, LOW); 
-    shiftOut(SER1, SRCLK1, LSBFIRST, B00011000);  
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B01111110);  
-    digitalWrite(RCLK1, HIGH); 
+    int matriz[8][8];
+
+    // Llenar la matriz con espacios en blanco
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            matriz[i][j] = 0;
+        }
+    }
+
+    // Colocar la "X" en la matriz
+    for (int i = 0; i < 8; i++) {
+        matriz[i][i] = 1;
+        matriz[i][7 - i] = 1;
+    }
+
+    // Declarar un arreglo de variables para almacenar los números decimales
+    int numerosDecimales[8];
+
+    // Calcular y almacenar los números decimales
+    for (int i = 0; i < 8; i++) {
+        numerosDecimales[i] = binarioADecimal(matriz[i], 8);
+    }
+  	//Mustra en los leds el patron
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST,numerosDecimales[0]  );  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 254 );  
+      digitalWrite(RCLK1, HIGH); 
+
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST,numerosDecimales[1]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST,  253);  
+      digitalWrite(RCLK1, HIGH); 
   
- 	digitalWrite(RCLK1, LOW);
-   	shiftOut(SER1, SRCLK1, LSBFIRST, B00111100); 
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B10111101);
-    digitalWrite(RCLK1, HIGH); 
-     
-    digitalWrite(RCLK1, LOW); 
-    shiftOut(SER1, SRCLK1, LSBFIRST, B01111110);  
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B11011011);  
-    digitalWrite(RCLK1, HIGH); 
-    
-    digitalWrite(RCLK1, LOW); 
-    shiftOut(SER1, SRCLK1, LSBFIRST, B11111111);  
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B11100111);  
-    digitalWrite(RCLK1, HIGH); 
-   
-}
-void patron3() {  
-    
-	digitalWrite(RCLK1, LOW); 
-    shiftOut(SER1, SRCLK1, LSBFIRST, B11011011);  
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B00111111);  
-    digitalWrite(RCLK1, HIGH); 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[2]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 251 );  
+      digitalWrite(RCLK1, HIGH);
+
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[3]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 247 );  
+      digitalWrite(RCLK1, HIGH); 
+ 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[4]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST,239 );  
+      digitalWrite(RCLK1, HIGH); 
   
- 	digitalWrite(RCLK1, LOW);
-   	shiftOut(SER1, SRCLK1, LSBFIRST, B01101101); 
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B11001111);
-    digitalWrite(RCLK1, HIGH); 
-     
-    digitalWrite(RCLK1, LOW); 
-    shiftOut(SER1, SRCLK1, LSBFIRST, B11011011); 
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B11110011);  
-    digitalWrite(RCLK1, HIGH); 
-    
-    digitalWrite(RCLK1, LOW); 
-    shiftOut(SER1, SRCLK1, LSBFIRST, B01101101);  
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B11111100);  
-    digitalWrite(RCLK1, HIGH); 
-   
-}
-void patron4() {  
-    
-	digitalWrite(RCLK1, LOW); 
-    shiftOut(SER1, SRCLK1, LSBFIRST, B11110000);  
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B01111110);  
-    digitalWrite(RCLK1, HIGH); 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[5]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 223);  
+      digitalWrite(RCLK1, HIGH); 
   
- 	digitalWrite(RCLK1, LOW);
-   	shiftOut(SER1, SRCLK1, LSBFIRST, B01111000); 
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B10111101);
-    digitalWrite(RCLK1, HIGH); 
-     
-    digitalWrite(RCLK1, LOW); 
-    shiftOut(SER1, SRCLK1, LSBFIRST, B00111100); 
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B11011011);  
-    digitalWrite(RCLK1, HIGH); 
-    
-    digitalWrite(RCLK1, LOW); 
-    shiftOut(SER1, SRCLK1, LSBFIRST, B00011110);  
-  	shiftOut(SER1, SRCLK1, MSBFIRST, B11100111);  
-    digitalWrite(RCLK1, HIGH); 
-   
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[6]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST,191 );  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, numerosDecimales[7]);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 127);  
+      digitalWrite(RCLK1, HIGH); 
+
 }
+void patron1(){
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST,  B00011000);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 254 );  
+      digitalWrite(RCLK1, HIGH); 
 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, B00111100);  
+      shiftOut(SER1, SRCLK1, LSBFIRST,  253);  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, B01111110);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 251 );  
+      digitalWrite(RCLK1, HIGH);
 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, B11111111);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 247 );  
+      digitalWrite(RCLK1, HIGH); 
+ 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, B11111111);  
+      shiftOut(SER1, SRCLK1, LSBFIRST,239 );  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, B01111110);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 223);  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, B00111100);  
+      shiftOut(SER1, SRCLK1, LSBFIRST,191 );  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, MSBFIRST, B00011000);  
+      shiftOut(SER1, SRCLK1, LSBFIRST, 127);  
+      digitalWrite(RCLK1, HIGH); 
+}
+void patron3(){
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST,  B11011011);  
+      shiftOut(SER1, SRCLK1, MSBFIRST,  B01111111);  
+      digitalWrite(RCLK1, HIGH); 
 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B11011011);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, B10111111);  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B01101101);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, B11011111);  
+      digitalWrite(RCLK1, HIGH);
 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B01101101);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, B11101111);  
+      digitalWrite(RCLK1, HIGH); 
+ 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B11011011);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, B11110111);  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B11011011);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, B11111011);  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B01101101);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, B11111101);  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B01101101);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, B11111110);  
+      digitalWrite(RCLK1, HIGH); 
+}
+void patron4(){
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST,  B11110000);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, 254 );  
+      digitalWrite(RCLK1, HIGH); 
 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B01111000);  
+      shiftOut(SER1, SRCLK1, MSBFIRST,  253);  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B00111100);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, 251 );  
+      digitalWrite(RCLK1, HIGH);
+
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B00011110);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, 247 );  
+      digitalWrite(RCLK1, HIGH); 
+ 
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B00011110);  
+      shiftOut(SER1, SRCLK1, MSBFIRST,239 );  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B00111100);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, 223);  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B01111000);  
+      shiftOut(SER1, SRCLK1, MSBFIRST,191 );  
+      digitalWrite(RCLK1, HIGH); 
+  
+      digitalWrite(RCLK1, LOW); 
+      shiftOut(SER1, SRCLK1, LSBFIRST, B11110000);  
+      shiftOut(SER1, SRCLK1, MSBFIRST, 127);  
+      digitalWrite(RCLK1, HIGH); 
+}
 
